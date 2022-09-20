2bf5e894b9823c5004d92a0c8ff8d89c310921d6
Vladimir Zdravkovski <vladimir.zdravkovski@alite-international.com>
2022-09-20 14:01:10 +0200

    Remove api key

--- lib/global/map_key.dart
+++ lib/global/map_key.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:users_app/models/allUsers.dart';
 
-String mapKey = "AIzaSyCAN0Hsn9u216v8ovPRWWlYtX7jXsNurfw";
+String mapKey = "";
 
 User? firebaseUser;
 
